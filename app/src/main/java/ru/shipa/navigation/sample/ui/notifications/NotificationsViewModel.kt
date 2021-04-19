abae6f303aaaa730e241af363622b9ad615753ab
Shipugin Vladislav <Vladislav.SHIPUGIN@raiffeisen.ru>
2021-04-19 14:08:23 +0300

    Create project.

---
+++ app/src/main/java/ru/shipa/navigation/sample/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package ru.shipa.navigation.sample.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
